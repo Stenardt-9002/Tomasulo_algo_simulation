a86e5e4661b6abdd838b8112724c29d2a40add07
Stenardt-9002 <esd17i014@iiitdm.ac.in>
2020-06-27 19:59:16 +0530

    DeBUG MODE Added

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 import os
 # import finalparser
 
+DEBUG = False
+
+
 
 if __name__ == '__main__':
-    if os.name == 'nt':
-        try:
-            os.system("python finalparser.py")
-        except:
-            print("An unexpected Error occured,Program Halted,Report Input to user OR check python installation in O.S.")
+    if DEBUG == False:
+        if os.name == 'nt':
+            try:
+                os.system("python finalparser.py")
+            except:
+                print("An unexpected Error occured,Program Halted,Report Input to user OR check python installation in O.S.")
 
 
         pass
         #windows
-    else:
-        try:
-            os.system("python3 finalparser.py")
-        except:
-            print("An unexpected Error occured,Program Halted,Report Input to user")
+        else:
+            try:
+                os.system("python3 finalparser.py")
+            except:
+                print("An unexpected Error occured,Program Halted,Report Input to user")
 
-        pass
+    else:
+        os.system("python3 debuggerfile.py")
