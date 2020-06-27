8ccf967f489cf95f0c7be07a726fe371d8249523
Stenardt-9002 <esd17i014@iiitdm.ac.in>
2020-06-27 15:50:56 +0530

    Deassembler and parsed text added

---
+++ matrixforsimul.cpp
@@ -0,0 +1,86 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int arr1[3][3];
+	int arr2[3][3];
+	int arr3[3][3];
+
+
+for(int i=0;i<3;i++)
+{
+	for(int j=0;j<3;j++)
+	{
+		cin>>arr1[i][j];
+	}
+}
+
+
+
+
+//for(int i=0;i<3;i++)
+//{
+//	cout<<"\n";
+//	for(int j=0;j<3;j++)
+//	{
+////		cin>>arr1[i][j];
+//		cout<<arr1[i][j]<<" ";
+//	}
+//}
+
+
+
+for(int i=0;i<3;i++)
+{
+	for(int j=0;j<3;j++)
+	{
+		cin>>arr2[i][j];
+	}
+}
+
+
+
+
+//for(int i=0;i<3;i++)
+//{
+//	cout<<"\n";
+//	for(int j=0;j<3;j++)
+//	{
+////		cin>>arr1[i][j];
+//		cout<<arr1[i][j]<<" ";
+//	}
+//}
+//
+   for (int i = 0; i < 3; i++) 
+    { 
+        for (int j = 0; j < 3; j++) 
+        { 
+            arr3[i][j] = 0; 
+            for (int k = 0; k < 3; k++) 
+            { 
+                arr3[i][j] += arr1[i][k]*arr2[k][j]; 
+            } 
+        } 
+    } 
+
+
+
+
+
+
+
+for(int i=0;i<3;i++)
+{
+	cout<<"\n";
+	for(int j=0;j<3;j++)
+	{
+//		cin>>arr1[i][j];
+		cout<<arr3[i][j]<<" ";
+	}
+}
+
+
+
+
+return 0;
+}
---
+++ newmatrix.cpp
@@ -0,0 +1,26 @@
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+
+	int x1 = 1,x2 = 2,x3 = 3,x4 = 4,x5 = 5,x6 = 6,x7 = 7,x8 = 8,x9 = 9;
+	int y1 = 1,y2 = 2,y3 = 3,y4 = 4,y5 = 5,y6 = 6,y7 = 7,y8 = 8,y9 = 9;
+	int z1 = x1*y1+x2*y4+x3*y7;
+	int z2 = x1*y2+x2*y5+x3*y8;
+	int z3 = x1*y3+x2*y6+x3*y9;
+	int z4 = x4*y1+x5*y4+x6*y7;
+	int z5 = x4*y2+x5*y5+x6*y8;
+	int z6 = x4*y3+x5*y6+x3*y9;
+	int z7 = x7*y1+x8*y4+x9*y7;
+	int z8 = x7*y2+x8*y5+x9*y8;
+	int z9 = x7*y3+x8*y6+x9*y9;
+
+
+}
+
+
+
+
